aa4d5cc30f86a1d1954c83bc6b7bbbb69c697e89
Dnam00 <hyunda@live.unc.edu>
2022-09-02 02:10:43 -0400

    added node_modules again

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var http = require('http');
 // Require fs module
 var fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
-const args = require('minimist')(process.argv.slice(2));
+var mini = require('minimist');
+var args = require('minimist')(process.argv.slice(2));
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-//args.port;
+//var args = .port;
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = args.port || process.env.PORT || 3000;
